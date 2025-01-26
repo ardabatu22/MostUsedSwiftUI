562c4d7dc6c6e3bdf7c347623d73a4e90f474b32
Batuhan Arda <arda.batu@icloud.com>
2025-01-27 01:09:17 +0300

    3 button type

--- MostUsedInSwiftUI/MostUsedInSwiftUI/Sources/Buttons/ButtonType.swift
+++ MostUsedInSwiftUI/MostUsedInSwiftUI/Sources/Buttons/ButtonType.swift
@@ -9,9 +9,37 @@ import SwiftUI
 
 struct ButtonType: View {
     var body: some View {
+        
+        // This is basic button type we can just click on the text
         Button("Test"){
-            print("Action")
+            print("Action1")
+        }.padding()
+        .background(.red)
+        .cornerRadius(15)
+
+        // I usually use this one
+        Button{
+          print("action2")
+        }label:{
+            Text("Click")
+                .foregroundStyle(.black)
+                .padding()
+                .background(.red)
+                .cornerRadius(15)
         }
+        
+        // Button 2 and 3 same
+        Button(action: {
+            print("action3")
+        }) {
+            Text("Action Button")
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+        }
+
+        
     }
 }
 

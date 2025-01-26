a54e1d8388596b8577af9f78f009b8216d77c689
Batuhan Arda <arda.batu@icloud.com>
2025-01-27 00:53:09 +0300

    added button type

---
+++ MostUsedInSwiftUI/MostUsedInSwiftUI/MostUsedInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MostUsedInSwiftUIApp.swift
+//  MostUsedInSwiftUI
+//
+//  Created by Batuhan Arda on 25.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MostUsedInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MostUsedInSwiftUI/MostUsedInSwiftUI/Sources/Buttons/ButtonType.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonType.swift
+//  MostUsedInSwiftUI
+//
+//  Created by Batuhan Arda on 27.01.2025.
+//
+
+import SwiftUI
+
+struct ButtonType: View {
+    var body: some View {
+        Button("Test"){
+            print("Action")
+        }
+    }
+}
+
+#Preview {
+    ButtonType()
+}
